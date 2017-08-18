a7885f0952cb2eeeedfa0a6636336d0ad3e92b20
Pablo <fixwah@gmail.com>
2017-08-18 16:19:42 -0300

    Imported files from Framework repository

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * User: pablo <fixwah@gmail.com>
+ * Date: 18/08/2017
+ * Time: 03:38 PM
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+
+$app = new Black\Application();
+$app->init();
